4573b351acee8f26fcd404d7e568566006be9a4b
Daehan Kim <really@iamghost.kr>
2022-02-07 23:43:19 +0900

    Added resizing and smart speed adjust fallback option

--- README.md
+++ README.md
@@ -1,16 +1,7 @@
 gif2tgsticker
 ====================
 
-Tool for converting gif to Telegram Sticker(webm)
-
-Introduction
---------------------
-
-Telegram added [Video Sticker Support](https://core.telegram.org/stickers#video-sticker-requirements), This tool tries to:
-
-* Convert video to WebM
-* Resize video within 512px and keeping aspect ratio
-* Speed up video if it's longer than 3 seconds
+Tool for converting gif to WebM video for [Telegram Video Sticker](https://core.telegram.org/stickers#video-sticker-requirements)
 
 How to use
 --------------------
@@ -26,6 +17,41 @@ Download [gif2tgsticker.exe from releases](../../releases) and put together with
 poetry install
 poetry run python gif2tgsticker.py
 ```
+
+Options
+--------------------
+
+### FPS
+
+Telegram supports up to 30 FPS.
+
+Note that Video Stickers have 256KB file size limit, lowering framerate can help for fitting.
+
+### Resize mode
+
+Since Telegram requires video to have 512px on one side, it tries to adjust it.
+
+  * Scale: Scale up/down image itself.
+    * Landscape video will have 512px width, height will be resized within aspect ratio.
+    * Vertical video will have 512px height, width will be resized within aspect ratio.
+  * Pad: Add transparent padding
+    * Landscape video will have left, right padding.
+    * Vertical video will have top, bottom padding.
+
+
+### Smart speed adjust duration limit
+
+Telegram Video Sticker have 3 seconds duration limit.
+
+If the video is longer than 3 seconds, this tool will try to fit this duration by speeding up playback.
+
+
+### Smart speed adjust fallback PTS modifier
+
+If this tool is unable to get playback duration from file, this value will be used as PTS(Presentation Time Stamp) modifier.
+
+* 1.0 means 1x playback speed
+* 0.5 means 2x playback speed
 
 
 Dependencies
--- gif2tgsticker.py
+++ gif2tgsticker.py
@@ -8,7 +8,10 @@ import ffmpeg
 import tkinterdnd2 as tkd
 
 DEFAULT_FPS = 30
-DEFAULT_DURATION_LIMIT = 175
+DEFAULT_SMART_DURATION_LIMIT = '2.9'
+DEFAULT_RESIZE_MODE = 'scale'
+DEFAULT_SPEED_ADJUST_MODE = 'smart'
+DEFAULT_FALLBACK_PTS = '1.0'
 
 root = tkd.TkinterDnD.Tk()
 root.withdraw()
@@ -18,8 +21,17 @@ root.grid_columnconfigure(0, minsize=600)
 fps_var = tk.IntVar()
 fps_var.set(DEFAULT_FPS)
 
-limit_duration_var = tk.IntVar()
-limit_duration_var.set(DEFAULT_DURATION_LIMIT)
+speed_adjust_mode_var = tk.StringVar()
+speed_adjust_mode_var.set(DEFAULT_SPEED_ADJUST_MODE)
+
+smart_limit_duration_var = tk.StringVar()
+smart_limit_duration_var.set(DEFAULT_SMART_DURATION_LIMIT)
+
+fallback_pts_var = tk.StringVar()
+fallback_pts_var.set(DEFAULT_FALLBACK_PTS)
+
+resize_mode_var = tk.StringVar()
+resize_mode_var.set(DEFAULT_RESIZE_MODE)
 
 option_box = tk.Frame(root)
 option_box.grid(row=0, column=0)
@@ -29,17 +41,33 @@ tk.Label(option_box, text="FPS:")\
 tk.Entry(option_box, textvariable=fps_var)\
   .grid(row=0, column=1)
 
-tk.Label(option_box, text="Limit duration to(if over 3 seconds):")\
+
+tk.Label(option_box, text="Resize mode:")\
   .grid(row=1, column=0)
-tk.Entry(option_box, textvariable=limit_duration_var)\
-  .grid(row=1, column=1)
+
+radio_box = tk.Frame(option_box)
+radio_box.grid(row=1, column=1)
+
+tk.Radiobutton(radio_box, text='Scale', value='scale', variable=resize_mode_var)\
+  .grid(row=0, column=0)
+tk.Radiobutton(radio_box, text='Pad', value='pad', variable=resize_mode_var)\
+  .grid(row=0, column=1)
+
+tk.Label(option_box, text="Smart speed adjust duration limit\n(when video is longer than 3 seconds):")\
+  .grid(row=2, column=0)
+tk.Entry(option_box, textvariable=smart_limit_duration_var)\
+  .grid(row=2, column=1)
+
+tk.Label(option_box, text="Smart speed adjust fallback PTS modifier\n(Used when unable to get duration from video)\n(0.5 = 2x speed):")\
+  .grid(row=3, column=0)
+tk.Entry(option_box, textvariable=fallback_pts_var)\
+  .grid(row=3, column=1)
 
 tk.Label(root, text='Drag and drop files here:')\
-  .grid(row=1, column=0, padx=10, pady=5)
-
+  .grid(row=4, column=0, padx=10, pady=5)
 
 listbox = tk.Listbox(root, width=1, height=20)
-listbox.grid(row=2, column=0, padx=5, pady=5, sticky='news')
+listbox.grid(row=5, column=0, padx=5, pady=5, sticky='news')
 
 def drop(event):
     if event.data:
@@ -80,18 +108,29 @@ def process_file(filepath):
     stream = info['streams'][0]
     fmt = info['format']
 
-    # Try to scale to 512px
-    if stream['width'] >= stream['height']:
-        job = job.filter('scale', 512, -1)
-    else:
-        job = job.filter('scale', -1, 512)
+    if resize_mode_var.get() == 'scale':
+        # Try to scale to 512px
+        if stream['width'] >= stream['height']:
+            job = job.filter('scale', 512, -1)
+        else:
+            job = job.filter('scale', -1, 512)
+
+    elif resize_mode_var.get() == 'pad':
+        if stream['width'] >= stream['height']:
+            job = job.filter('pad', width=512, height='min(ih,512)', x='(ow-iw)/2', y='(oh-ih)/2', color="white@0")
+        else:
+            job = job.filter('pad', width='min(iw,512)', height=512, x='(ow-iw)/2', y='(oh-ih)/2', color="white@0")
+
 
     if 'duration' in fmt:
         duration = float(fmt['duration'])
 
         # Try speed up video if it's over 3 seconds
         if duration > 3.0:
-            job = job.filter('setpts', f"({limit_duration_var.get()}/{duration * 60})*PTS")
+            job = job.filter('setpts', f"({smart_limit_duration_var.get()}/{duration})*PTS")
+    else:
+        job = job.filter('setpts', f"{fallback_pts_var.get()}*PTS")
+
 
     out_path = str(p.with_suffix('.webm'))
 
